7545d8ca145030627ce16348658cb0a746df1b4a
lobotomizedDev <oskar.rochowiak@tutanota.com>
2024-02-18 21:51:07 +0100

    switched to x11rb as it is documented way better

--- src/x11.rs
+++ src/x11.rs
@@ -1,90 +1,102 @@
 use crate::helpers::resize_image;
-use image::*;
-use std::{error::Error, sync::mpsc};
-use xcb::{shm, x};
+use image::RgbImage;
+use std::{
+    error::Error,
+    fs::{remove_file, OpenOptions},
+    io::Write,
+    sync::mpsc,
+};
+use x11rb::{
+    connect,
+    connection::Connection,
+    protocol::{
+        shm,
+        xproto::{
+            AtomEnum, ChangeWindowAttributesAux, CloseDown, ConnectionExt, CreateGCAux, Kill,
+            PropMode,
+        },
+    },
+};
 
 const ATOMS: &'static [&'static str] = &["_XROOTPMAP_ID", "_XSETROOT_ID", "ESETROOT_PMAP_ID"];
+const TEMP_FILE_CONTENT: [u8; 8] = [0x00, 0x01, 0x02, 0x03, 0xff, 0xfe, 0xfd, 0xfc];
+
+fn make_file() -> std::io::Result<std::fs::File> {
+    let file_name = "shared_memory.bin";
+    let mut file = OpenOptions::new()
+        .create(true)
+        .read(true)
+        .write(true)
+        .truncate(true)
+        .open(file_name)?;
+    file.write_all(&TEMP_FILE_CONTENT)?;
+    remove_file(file_name)?;
+    Ok(file)
+}
 
 pub fn x11(rx: mpsc::Receiver<RgbImage>) -> Result<(), Box<dyn Error>> {
-    let (conn, screen_num) = xcb::Connection::connect(None).expect("Failed to connect to X server");
+    let file = make_file()?;
+    let (conn, screen_num) = connect(None).expect("Failed to connect to X server");
+    let screen = conn.setup().roots[screen_num].clone();
 
-    let setup = &conn.get_setup();
-    let mut screen_iter = setup.roots();
-    let screen = screen_iter
-        .nth(screen_num as usize)
-        .expect("Failed to get screen info");
+    let width = screen.width_in_pixels;
+    let height = screen.height_in_pixels;
 
-    let width = screen.width_in_pixels();
-    let height = screen.height_in_pixels();
+    let shmseg = conn.generate_id()?;
+    shm::create_segment(&conn, shmseg, width as u32 * height as u32 * 4, false)?;
+    shm::attach_fd(&conn, shmseg, file, false)?;
 
-    // Create shared memory
-
-    let pixmap = conn.generate_id();
-    conn.check_request(conn.send_request_checked(&x::CreatePixmap {
-        depth: screen.root_depth(),
-        pid: pixmap,
-        drawable: x::Drawable::Window(screen.root()),
+    let pixmap = conn.generate_id()?;
+    shm::create_pixmap(
+        &conn,
+        pixmap,
+        screen.root,
         width,
         height,
-    }))?;
+        screen.root_depth,
+        shmseg,
+        0,
+    )?;
 
-    let context = conn.generate_id();
-    conn.check_request(conn.send_request_checked(&x::CreateGc {
-        cid: context,
-        drawable: x::Drawable::Pixmap(pixmap),
-        value_list: &[],
-    }))?;
+    let context = conn.generate_id()?;
+    conn.create_gc(context, pixmap, &CreateGCAux::new())?;
+    conn.flush()?;
 
     loop {
         let image = rx.recv()?;
-        let image = resize_image(image, width as u32, height as u32)?;
+        let _image = resize_image(image, width as u32, height as u32)?;
 
-        // Put the image onto the shared memory segment
+        // Put image into shared memory
 
-        conn.check_request(conn.send_request_checked(&x::KillClient {
-            resource: x::Kill::AllTemporary as u32,
-        }))?;
-
-        conn.check_request(conn.send_request_checked(&x::SetCloseDownMode {
-            mode: x::CloseDown::RetainTemporary,
-        }))?;
+        conn.kill_client(Kill::ALL_TEMPORARY)?;
+        conn.set_close_down_mode(CloseDown::RETAIN_TEMPORARY)?;
 
         ATOMS.iter().for_each(|atom| {
             let atom = conn
-                .wait_for_reply(conn.send_request(&x::InternAtom {
-                    only_if_exists: false,
-                    name: atom.as_bytes(),
-                }))
+                .intern_atom(false, atom.as_bytes())
                 .unwrap()
-                .atom();
+                .reply()
+                .unwrap()
+                .atom;
 
-            conn.send_request_checked(&x::ChangeProperty {
-                mode: x::PropMode::Replace,
-                window: screen.root(),
-                property: atom,
-                r#type: x::ATOM_PIXMAP,
-                data: &[screen.root()],
-            });
+            conn.change_property(
+                PropMode::REPLACE,
+                screen.root,
+                atom,
+                AtomEnum::PIXMAP,
+                32,
+                1,
+                &[screen.root as u8],
+            )
+            .unwrap();
         });
 
-        conn.check_request(conn.send_request_checked(&x::KillClient {
-            resource: x::Kill::AllTemporary as u32,
-        }))?;
-        conn.check_request(conn.send_request_checked(&x::SetCloseDownMode {
-            mode: x::CloseDown::RetainTemporary,
-        }))?;
-        conn.check_request(conn.send_request_checked(&x::ChangeWindowAttributes {
-            window: screen.root(),
-            value_list: &[x::Cw::BackPixmap(pixmap)],
-        }))?;
-        conn.check_request(conn.send_request_checked(&x::ClearArea {
-            exposures: false,
-            window: screen.root(),
-            x: 0,
-            y: 0,
-            width,
-            height,
-        }))?;
-        conn.flush()?;
+        conn.kill_client(Kill::ALL_TEMPORARY)?;
+        conn.set_close_down_mode(CloseDown::RETAIN_TEMPORARY)?;
+        conn.change_window_attributes(
+            screen.root,
+            &ChangeWindowAttributesAux::new().background_pixmap(pixmap),
+        )?;
+        conn.clear_area(false, screen.root, 0, 0, width, height)?;
     }
 }
